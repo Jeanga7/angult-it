de0c76aac92d9df20e8a4fe278cd666c5334d573
jgoudiab <jeangabrielgoudiaby@gmail.com>
2025-03-11 19:09:43 +0000

    init

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CaptchaComponent } from './captcha/captcha.component';
+import { ResultComponent } from './result/result.component';
+
+export const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'captcha', component: CaptchaComponent },
+  { path: 'result', component: ResultComponent },
+];
